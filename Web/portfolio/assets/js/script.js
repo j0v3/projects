06bfd1aa5acc611b14ad14ba47fef407f9026c1e
j0v3 <201791647+j0v3@users.noreply.github.com>
2025-11-14 09:57:03 -0300

    Atualizando

---
+++ Web/portfolio/assets/js/script.js
@@ -0,0 +1,14 @@
+const serviceBlockButton = document.querySelectorAll(".serviceBlockButton");
+const serviceInformations = document.querySelectorAll(".serviceInformations");
+
+console.log(serviceBlockButton);
+console.log(serviceInformations);
+
+switch (serviceBlockButton){
+
+    case serviceBlockButton[0]: 
+        serviceInformations[0].classList.add("active") ;
+        break;
+    default: NamedNodeMap;
+
+}
