2f4d659dbec7ddb4ec8c4160f9f93307a571720a
j0v3 <201791647+j0v3@users.noreply.github.com>
2025-11-18 15:17:26 -0300

    novo sistema de interatividade, nova contagem de pontos e inserção de nomes

--- Web/counterpointsvolei/assets/js/contadores.js
+++ Web/counterpointsvolei/assets/js/contadores.js
@@ -1,104 +1,132 @@
-/**section A */
 buttonMinusA = document.querySelector("#buttonMinusA");
 counterPointsA = document.querySelector("#counterPointsA");
-buttonPlusA = document.querySelector(".buttonPlusA");
-containerSetA = document.querySelector("#containerSetA");
-nameEditA = document.querySelector("#nameEditA");
 
+containerSetA = document.querySelector("#containerSetAN");
+containerSetB = document.querySelector("#containerSetBN");
 
-/**viewSituation = document.querySelector("#viewSituation");*/
-insertTeams = document.querySelector(".insertTeams");
-
-/**section B */
 buttonMinusB = document.querySelector("#buttonMinusB");
 counterPointsB = document.querySelector("#counterPointsB");
-buttonPlusB = document.querySelector(".buttonPlusB");
-containerSetB = document.querySelector("#containerSetB");
+
+insertTeams = document.querySelector(".insertTeams");
+
+timeA = document.querySelector("#timeA");
+timeB = document.querySelector("#timeB");
+
+renameTeams = document.querySelector("#renameTeams");
+closePopupTeams = document.querySelector("#closePopupTeams");
+
+nameEditA = document.querySelector("#nameEditA");
 nameEditB = document.querySelector("#nameEditB");
 
-/**teamA, teamB, SetA, SetB */
+nameTeamA = document.querySelector("#nameTeamA");
+nameTeamB = document.querySelector("#nameTeamB");
 
-let valores = [0,0,0,0];
+/**teamA, teamB, SetA, SetB, valor para ganhar*/
+let valores = [0, 0, 0, 0, 25];
 let nameTeams = ["Time A", "Time B"];
 
-function plusElement(valor){
+function checkEnd() {
+  if (valores[0] === valores[4] - 1 && valores[1] === valores[4] - 1) {
+    valores[4] = valores[4] + 2;
+    return;
+  } else if (valores[0] === valores[4]) {
+    valores[2] = plus(valores[2]);
+    valores[0] = 0;
+    valores[1] = 0;
+    valores[4] = 25;
+    return;
+  } else if (valores[1] === valores[4]) {
+    valores[3] = plus(valores[3]);
+    valores[0] = 0;
+    valores[1] = 0;
+    valores[4] = 25;
+    return;
+  }
+}
 
-    let sum = valor +1;
-    return sum;
+function updateDisplay(valores) {
+  containerSetA.textContent = valores[2];
+  containerSetB.textContent = valores[3];
+  counterPointsA.textContent = valores[0];
+  counterPointsB.textContent = valores[1];
+}
+function plus(valor) {
+  return (valor = valor + 1);
+}
+function minus(valor) {
+  if (valor > 0) {
+    valor = valor - 1;
+    return valor;
+  } else {
+    return (valor = 0);
+  }
+}
 
-};
+function alteraNomes(nameTeams, timeA, timeB) {
+  if (timeA == null || timeA == "") {
+    nameTeams[0] = nameTeams[0];
+    return;
+  } else if (timeB == null || timeB == "") {
+    nameTeams[1] = nameTeams[1];
+    return;
+  } else {
+    nameTeams[0] = timeA;
+    nameTeams[1] = timeB;
+    return;
+  }
+}
 
-function minusElement(valor){
+function fechaAbrePopUp(popupActive){
 
-    if(valor < 0  || valor == 0){
+  popupActive.classList.toggle("active");
 
-        return 0;
+}
 
-    } else{
-        let minus = valor -1;
-        return minus;
+function exibeNovosNomes(nameTeams) {
+  nameEditA.textContent = nameTeams[0];
+  nameEditB.textContent = nameTeams[1];
+  return;
+}
 
-    }
+counterPointsA.addEventListener("click", () => {
+  valores[0] = plus(valores[0]);
+  checkEnd();
+  updateDisplay(valores);
+});
+counterPointsB.addEventListener("click", () => {
+  valores[1] = plus(valores[1]);
+  checkEnd();
+  updateDisplay(valores);
+});
+buttonMinusA.addEventListener("click", () => {
+  valores[0] = minus(valores[0]);
+  checkEnd();
+  updateDisplay(valores);
+});
+buttonMinusB.addEventListener("click", () => {
+  valores[1] = minus(valores[1]);
+  checkEnd();
+  updateDisplay(valores);
+});
+renameTeams.addEventListener("click", ()=>{
 
-};
-
-
-
-/**TeamA */
-/**O valores[0] se refere aos pontos do TeamA */
-buttonMinusA.addEventListener("click",()=>{
-
-   valores[0] = minusElement(valores[0]);
-    counterPointsA.textContent = valores[0];
+  alteraNomes(nameTeams, timeA.value, timeB.value);
+  exibeNovosNomes(nameTeams);
+  fechaAbrePopUp(insertTeams);
 
 });
+closePopupTeams.addEventListener("click", ()=>{
 
-buttonPlusA.addEventListener("click",()=>{
-
-    if(valores[0] < 25){
-
-        valores[0] = plusElement(valores[0]);
-        counterPointsA.textContent = valores[0];
-
-    }else{
-
-        /**O valores[2] se refere aos pontos do set do time A */
-        valores[0] = 0
-        counterPointsA.textContent = valores[0];
-
-        valores[2] = plusElement(valores[2])
-        containerSetA.textContent = valores[2];
-
-    }
+  fechaAbrePopUp(insertTeams);
 
 });
+nameTeamA.addEventListener("click", ()=>{
 
-/**TeamB */
-/**O valores[1] se refere aos pontos do TeamB */
-buttonMinusB.addEventListener("click",()=>{
+  fechaAbrePopUp(insertTeams);
 
-   valores[1] = minusElement(valores[1]);
-    counterPointsB.textContent = valores[1];
+})
+nameTeamB.addEventListener("click", ()=>{
 
-});
+  fechaAbrePopUp(insertTeams);
 
-buttonPlusB.addEventListener("click",()=>{
-
-    if(valores[1] < 25){
-
-        valores[1] = plusElement(valores[1]);
-        counterPointsB.textContent = valores[1];
-
-    }else{
-
-        /**O valores[3] se refere aos pontos do set do time B */
-        valores[1] = 0
-        counterPointsB.textContent = valores[1];
-
-        valores[3] = plusElement(valores[3])
-        containerSetB.textContent = valores[3];
-
-    }
-
-});
-
+})
